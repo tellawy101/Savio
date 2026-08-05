2b097fe3a2ae39756c21c5ebf465cf66f9f84159
tellawy101 <mahmoudtelawy4@gmail.com>
2026-08-05 15:38:28 +0300

    update

--- js/script.js
+++ js/script.js
@@ -117,26 +117,43 @@ function renderExpenses() {
 `;
         expenseList.appendChild(li);
         let startX = 0;
-let currentX = 0;
-let offsetX = 0;
+        let currentX = 0;
+        let offsetX = 0;
+        let startY = 0;
 
 const card = li.querySelector(".expense-main");
 
 card.addEventListener("touchstart", (e) => {
     startX = e.touches[0].clientX;
+    startY = e.touches[0].clientY;
 });
 
 card.addEventListener("touchmove", (e) => {
-    
-    currentX = offsetX + (e.touches[0].clientX - startX);
-    
+
+    const deltaX = e.touches[0].clientX - startX;
+const deltaY = e.touches[0].clientY - startY;
+
+if (Math.abs(deltaY) > Math.abs(deltaX)) return;
+e.preventDefault();
+currentX = offsetX + deltaX;
+
     if (currentX > 80) currentX = 80;
     if (currentX < -80) currentX = -80;
-    
-    card.style.transform = `translateX(${currentX}px)`;
-    
-});
 
+    card.style.transform = `translateX(${currentX}px)`;
+    const leftAction = li.querySelector(".swipe-delete");
+const rightAction = li.querySelector(".swipe-edit");
+
+if (currentX > 0) {
+    leftAction.style.opacity = currentX / 80;
+    rightAction.style.opacity = 0;
+} else {
+    rightAction.style.opacity = Math.abs(currentX) / 80;
+    leftAction.style.opacity = 0;
+}
+card.style.transition = "none";
+
+}, { passive: false });
 card.addEventListener("touchend", () => {
     
     if (currentX > 50) {
@@ -152,11 +169,32 @@ card.addEventListener("touchend", () => {
         offsetX = 0;
         
     }
-    
+    card.style.transition = "transform .25s ease";
     card.style.transform = `translateX(${offsetX}px)`;
     
+    if (offsetX === 70) {
+    leftAction.style.opacity = 1;
+    rightAction.style.opacity = 0;
+} else if (offsetX === -70) {
+    rightAction.style.opacity = 1;
+    leftAction.style.opacity = 0;
+} else {
+    leftAction.style.opacity = 0;
+    rightAction.style.opacity = 0;
+}
     currentX = 0;
     
+});
+document.addEventListener("touchstart", (e) => {
+
+    if (!li.contains(e.target)) {
+
+        offsetX = 0;
+        currentX = 0;
+
+        card.style.transform = "translateX(0px)";
+    }
+
 });
 
 });
